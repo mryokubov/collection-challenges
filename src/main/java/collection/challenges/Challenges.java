de4a3b128610f504f5bd0aa02958d764000d3a05
TairovichJR <yokubov.omonjon@gmail.com>
2022-05-03 17:50:46 -0400

    challenges

---
+++ src/main/java/collection/challenges/Challenges.java
@@ -0,0 +1,74 @@
+package collection.challenges;
+
+import java.util.*;
+
+public class Challenges {
+
+
+    //You must complete the implementation of these methods so that unit test pass.
+
+    /*
+        Given an array nums, return a new array where only unique sorted values exist
+    */
+    public int[] uniqueValues(int[] nums){
+        return null;
+    }
+
+    /*
+        Given an array nums, return the max value. If the array is empty, simply return -1
+    */
+    public int maxFromArray(int[] nums){
+        return -1;
+    }
+
+    /*
+        Given a list of String elements, return a new list where there are only elements that have more than 3 chars
+    */
+    public List<String> charsOver3(List<String> list){
+        return null;
+    }
+
+    /*
+        Given two lists, return a new list where only unique elements from 2 lists exist
+    */
+    public List<Integer> addTwoLists(List<Integer> list1, List<Integer> list2){
+        return null;
+    }
+
+    /*
+        Given String str, return how many 'ab' repeats in this str. You can ignore case sensitivity
+    */
+    public int repeatAbCount(String str){
+        return -1;
+    }
+
+    /*
+        Given the argument list, return the difference between the largest and the smallest values.
+        In case of empty list, return -1
+    */
+    public int bigDiff(List<Integer> list){
+        return -1;
+    }
+
+    /*
+        Given two lists, calculate their averages separately and return the largest of them.
+        Return 0 in case both lists are empty
+    */
+    public double maxAverage(List<Integer> list1, List<Integer> list2){
+        return -1;
+    }
+
+    /*
+        Given a list of String values and leng, return the number of elements that have the same length as leng
+    */
+    public int wordsCount(List<String> list, int leng){
+        return 0;
+    }
+
+    /*
+        Given int num, return a list where it contains values from 0 to num (excluded)
+    */
+    public List<Integer> fillArr(int num){
+        return null;
+    }
+}
---
+++ src/test/java/collection/challenges/ChallengesTest.java
@@ -0,0 +1,137 @@
+package collection.challenges;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ChallengesTest {
+
+    private Challenges challenges;
+    private static final double DELTA = 1e-15;
+
+    @Before
+    public void setUp(){
+        this.challenges = new Challenges();
+    }
+
+    @Test
+    public void uniqueValuesTest(){
+
+        int[] nums = {1,2,1,3,4,6,5,6,7,3,4,2,3};
+        int[] expected = {1, 2, 3, 4, 6, 5, 7};
+        int[] actual = challenges.uniqueValues(nums);
+
+        Assert.assertTrue(actual!= null);
+        Assert.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void maxFromArrayTest(){
+        int[] nums = {1,2,1,3,4,6,5,6,7,3,4,2,3};
+        int expected = challenges.maxFromArray(nums);
+        int actual = 7;
+        Assert.assertEquals(expected, actual);
+
+        nums = new int[]{};
+        expected = challenges.maxFromArray(nums);
+        actual = -1;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void charsOver3Test(){
+        List<String > str = new ArrayList<>(List.of("abdfd","dsf","d","dsfasd","fde","dfdfg"));
+        List<String> expected = List.of("abdfd", "dsfasd","dfdfg");
+        List<String> actual = challenges.charsOver3(str);
+
+        Assert.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void charsOver3EmptyTest(){
+        List<String > str = new ArrayList<>();
+        List<String> expected = new ArrayList<>();
+        List<String> actual = challenges.charsOver3(str);
+
+        Assert.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void addTwoListsTest(){
+        List<Integer> list1 = new ArrayList<>(List.of(1,2,3,1,2,3,1,32,34,2));
+        List<Integer> list2 = new ArrayList<>(List.of(9,5,3,1,2,6,1,32,34,2));
+
+        List<Integer> actual = challenges.addTwoLists(list1, list2);
+
+        List<Integer> expected = new ArrayList<>(Arrays.asList(32, 1, 2, 34, 3, 5, 6, 9));
+
+        Assert.assertEquals(expected , actual);
+    }
+
+    @Test
+    public void repeatAbCountTest(){
+        int actual = challenges.repeatAbCount("fdabglksabjfldabdlfja");
+        Assert.assertEquals(3, actual);
+
+        actual = challenges.repeatAbCount("");
+        Assert.assertEquals(0, actual);
+
+        actual = challenges.repeatAbCount(null);
+        Assert.assertEquals(0, actual);
+
+    }
+
+    @Test
+    public void bigDiffTest(){
+        List<Integer> list = new ArrayList<>(Arrays.asList(10, 3, 5, 6));
+        int actual = challenges.bigDiff(list);
+
+        Assert.assertEquals(7, actual);
+
+        list = new ArrayList<>(Arrays.asList(7, 2, 10, 9));
+        actual = challenges.bigDiff(list);
+
+        Assert.assertEquals(8, actual);
+
+        list = new ArrayList<>();
+        actual = challenges.bigDiff(list);
+
+        Assert.assertEquals(-1, actual);
+
+    }
+
+    @Test
+    public void maxAverageTest(){
+
+        double actual = challenges.maxAverage(null, null);
+
+        Assert.assertEquals(0, actual, DELTA);
+
+        List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3)); //2
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(3,4,5)); // 4
+
+
+        actual = challenges.maxAverage(list1, list2);
+        Assert.assertEquals(4, actual, DELTA);
+
+    }
+
+    @Test
+    public void wordsCountTest(){
+        int actual = challenges.wordsCount(List.of("a", "bb", "b", "ab", "dfdf", "dsfasd", "", "dfd","ty"), 2);
+        Assert.assertEquals(3, actual);
+    }
+
+    @Test
+    public void fillArrTest(){
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0,1,2,3,4,5));
+        List<Integer> actual = challenges.fillArr(6);
+        Assert.assertEquals(expected, actual);
+    }
+
+}
